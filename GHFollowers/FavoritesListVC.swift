6b99543dfcc60b34803c2afe3a892f01b5538863
Priyank Shah <pcs36063@outlook.com>
2020-07-16 18:25:10 -0500

    [2 new files added (search and favoritesVC)]

--- GHFollowers/FavoritesListVC.swift
+++ GHFollowers/FavoritesListVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FavoritesListVC.swift
 //  GHFollowers
 //
 //  Created by Priyank Shah on 7/16/20.
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FavoritesListVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemGreen
     }
-}
 
+}
---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Priyank Shah on 7/16/20.
+//  Copyright © 2020 Shah Priyank. All rights reserved.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+}
