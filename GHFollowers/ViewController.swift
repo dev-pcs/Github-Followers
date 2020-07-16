811ea0a02315cc0ada6e7c3070da69b0ed81f56e
Priyank Shah <pcs36063@outlook.com>
2020-07-16 17:57:48 -0500

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Priyank Shah on 7/16/20.
+//  Copyright © 2020 Shah Priyank. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
