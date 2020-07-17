d2a780b48fee9d48f14d96881da7d5de853b4952
Priyank Shah <pcs36063@outlook.com>
2020-07-17 14:49:30 -0500

    [MVC applid and userFollower model created]

---
+++ GHFollowers/Model/Follower.swift
@@ -0,0 +1,14 @@
+//
+//  Follower.swift
+//  GHFollowers
+//
+//  Created by Priyank Shah on 7/17/20.
+//  Copyright © 2020 Shah Priyank. All rights reserved.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
