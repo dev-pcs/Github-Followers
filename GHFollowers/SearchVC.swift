9b1d8b5e03a64940ef67fef379de40807780b3de
Priyank Shah <pcs36063@outlook.com>
2020-07-16 22:30:25 -0500

    [CallToActionButton constraints added]

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -24,6 +24,7 @@ class SearchVC: UIViewController {
         view.backgroundColor = .systemBackground
         configureLogoImageView()
         configureTextField()
+        configureCallToActionButton()
     }
     
     
@@ -59,6 +60,21 @@ class SearchVC: UIViewController {
             usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
             usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
             usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    
+    //MARK: - Configure CallToActionButton constraints
+    
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        
+        NSLayoutConstraint.activate([
+        
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
