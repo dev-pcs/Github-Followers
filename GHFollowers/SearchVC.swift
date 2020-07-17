d54966513933b3e08ca566f360d93e2175fd4434
Priyank Shah <pcs36063@outlook.com>
2020-07-16 20:15:09 -0500

    [search screen componenst added]

--- GHFollowers/SearchVC.swift
+++ GHFollowers/SearchVC.swift
@@ -9,9 +9,35 @@
 import UIKit
 
 class SearchVC: UIViewController {
-
+    
+    //MARK: - All Component Variables
+    
+    let logoimageView       = UIImageView()
+    let usernameTextField   = GFTextField()
+    let callToActionButton  = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
+    
+    
+    //MARK: - When View is Loaded
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        view.backgroundColor = .systemBackground
+    }
+    
+    
+    //MARK: - configure screen and constraints
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    
+    func configureLogoImageView() {
+        view.addSubview(logoimageView)
+        logoimageView.adjustsImageSizeForAccessibilityContentSizeCategory = false
+        logoimageView.image = UIImage(named: "gh-logo")
+        
+        
     }
 }
