b9955bbdc4c9deaacd7898e1bb31d6fc2428421c
Priyank Shah <pcs36063@outlook.com>
2020-07-16 19:27:19 -0500

    [Custom GF button created]

---
+++ GHFollowers/GFButton.swift
@@ -0,0 +1,44 @@
+//
+//  GFButton.swift
+//  GHFollowers
+//
+//  Created by Priyank Shah on 7/16/20.
+//  Copyright © 2020 Shah Priyank. All rights reserved.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    //MARK: - Button Initialization
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Usage Initializer with Args
+    
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    //MARK: - Button Layout
+    
+    
+    private func configure() {
+        layer.cornerRadius      = 10
+        titleLabel?.textColor   = .white
+        titleLabel?.font        = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
