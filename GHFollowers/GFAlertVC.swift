f1b288ffa595253d82ac2ad7d372a7bd81434bd8
Priyank Shah <pcs36063@outlook.com>
2020-07-17 10:43:18 -0500

    [Alert VC created along with Containerview ]

---
+++ GHFollowers/GFAlertVC.swift
@@ -0,0 +1,60 @@
+//
+//  GFAlertVC.swift
+//  GHFollowers
+//
+//  Created by Priyank Shah on 7/17/20.
+//  Copyright © 2020 Shah Priyank. All rights reserved.
+//
+
+import UIKit
+
+class GFAlertVC: UIViewController {
+
+    let containerView   = UIView()
+    let titleLabel      = GFTitleLabel(textAlignment: .center, fontSize: 20)
+    let messageLabel    = GFBodyLabel(textAlignment: .center)
+    let actionButton    = GFButton(backgroundColor: .systemPink, title: "Ok")
+    
+    var alertTitle: String?
+    var message: String?
+    var buttonTitle: String?
+    
+    init(title: String, message: String, ButtonTitle: String) {
+        super.init(nibName: nil, bundle: nil)
+        self.alertTitle     = title
+        self.message        = message
+        self.buttonTitle    = ButtonTitle
+    }
+    
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        configureContainerView()
+        
+    }
+    
+    
+    func configureContainerView() {
+        view.addSubview(containerView)
+        containerView.backgroundColor       = .systemBackground
+        containerView.layer.cornerRadius    = 16
+        containerView.layer.borderWidth     = 2
+        containerView.layer.borderColor     = UIColor.white.cgColor
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            containerView.widthAnchor.constraint(equalToConstant: 280),
+            containerView.heightAnchor.constraint(equalToConstant: 220)
+        ])
+    }
+    
+}
