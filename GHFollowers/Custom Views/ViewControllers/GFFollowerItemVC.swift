7bc8f06ada0688b059216a2db1ef57ea90359867
Priyank Shah <pcs36063@outlook.com>
2020-07-19 18:12:21 -0500

    [another subclass created]

---
+++ GHFollowers/Custom Views/ViewControllers/GFFollowerItemVC.swift
@@ -0,0 +1,26 @@
+//
+//  GFFollowerItemVC.swift
+//  GHFollowers
+//
+//  Created by Priyank Shah on 7/19/20.
+//  Copyright © 2020 Shah Priyank. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
