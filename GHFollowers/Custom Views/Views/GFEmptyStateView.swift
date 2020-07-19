ec610e5266bef91fad9fb9ae3669941e58f43efc
Priyank Shah <pcs36063@outlook.com>
2020-07-18 21:25:11 -0500

    [Project organization and emptyscreen]

---
+++ GHFollowers/Custom Views/Views/GFEmptyStateView.swift
@@ -0,0 +1,14 @@
+//
+//  GFEmptyStateView.swift
+//  GHFollowers
+//
+//  Created by Priyank Shah on 7/18/20.
+//  Copyright © 2020 Shah Priyank. All rights reserved.
+//
+
+import UIKit
+
+class GFEmptyStateView: UIView {
+    
+
+}
