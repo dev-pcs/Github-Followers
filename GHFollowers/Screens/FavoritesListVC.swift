1eeb844c8989cc5d669ba9dcd10faa101083d342
Priyank Shah <pcs36063@outlook.com>
2020-07-20 15:33:38 -0500

    [Swipe to delete added]

--- GHFollowers/Screens/FavoritesListVC.swift
+++ GHFollowers/Screens/FavoritesListVC.swift
@@ -95,4 +95,15 @@ extension FavoritesListVC: UITableViewDataSource, UITableViewDelegate {
     }
     
     
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else {return}
+        let favorite = favorites[indexPath.row]
+        favorites.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .left)
+        persistanceManager.updateWith(favorite: favorite, actionType: .remove) { [weak self] error in
+            guard let self = self else {return}
+            guard let error = error else {return}
+            self.presentGFAlertOnMainThread(title: "Unable to remove", message: error.rawValue, buttonTitle: "Ok")
+        }
+    }
 }
